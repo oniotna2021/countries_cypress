bad89b02bff48f94b8e8faab64c80ebf2fb14cce
AntonioMogollon <tonyusb@gmail.com>
2022-04-11 17:34:31 -0500

    1104 2

--- cypress/integration/Curso_cypress_UDEMY/segundoTest.js
+++ cypress/integration/Curso_cypress_UDEMY/segundoTest.js
@@ -8,7 +8,7 @@ describe('UDEMY second test',()=>{
         cy.viewport( 1280 , 720);
         cy.wait(500)
     })
-    it('second test',()=>{
+    it.only('second test',()=>{
         cy.on('uncaught exception',( err , runnable)=>{
             return false;
         })
@@ -66,7 +66,7 @@ describe('UDEMY second test',()=>{
             .type('Admin')
     })
 
-    it.only('sixth test: computers',()=>{
+    it('sixth test: computers',()=>{
         cy.visit('https://computer-database.gatling.io/computers/')
         cy.title().should('eq','Computers database')
         cy.wait(500)
@@ -79,5 +79,113 @@ describe('UDEMY second test',()=>{
         cy.xpath("/html/body/section/div[1]/form/input[1]")
         .clear()
     })
-    
+
+    it('seventh test: checkbox',()=>{
+        cy.on('uncaught exception',( err , runnable)=>{
+            return false;
+        })
+
+        cy.visit('https://demos.jquerymobile.com/1.4.5/checkboxradio-checkbox/')
+
+        cy.get("#ui-page-top > div.jqm-demos.ui-page.ui-page-theme-a.ui-page-footer-fixed.ui-page-active > div.ui-content.jqm-content > div:nth-child(6) > form > div > input[type=checkbox]")
+            .check({force:true})
+                .should('be.checked')
+        
+        cy.get('#ui-page-top > div.jqm-demos.ui-page.ui-page-theme-a.ui-page-footer-fixed.ui-page-active > div.ui-content.jqm-content > div:nth-child(7) > a')
+            .click({force:true})
+
+        cy.get('#ui-page-top > div.jqm-demos.ui-page.ui-page-theme-a.ui-page-footer-fixed.ui-page-active > div.ui-content.jqm-content > div:nth-child(12) > a')
+            .should('exist')
+            .and('be.visible')
+            .type('{enter}') //solucion cuando no hay donde hacer click
+        })
+
+        it.only('eight test: multiselect',()=>{
+
+            cy.visit('https://demos.jquerymobile.com/1.4.5/selectmenu/')
+
+            // cy.get('#select-native-1')
+            // .should('exist'
+            // .select(['The 1st Option', 'The 2nd Option'])
+            // para hacer select multiple
+        })
+
+        //importante promesa dentro de cypress.io
+
+        it.only('nine test: gogole with promise',()=>{
+
+            cy.visit('https://google.com')
+
+            cy.get('body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input')
+                .should('exist')
+                .and('be.visible')
+                .then('promise', ()=>{
+            cy.get('body > div.L3eUgb > div.o3j99.ikrT4e.om7nvf > form > div:nth-child(1) > div.A8SBwf > div.RNNXgb > div > div.a4bIc > input')
+                        .type('ferrari')
+                        .type('{enter}')
+            })
+
+           
+
+            // cy.get('#select-native-1')
+            // .should('exist'
+            // .select(['The 1st Option', 'The 2nd Option'])
+            // para hacer select multiple
+        })
+
+        it.only('test ten: asserts',()=>{
+            cy.visit('http://automationpractice.com/index.php')
+            cy.get('#block_top_menu > ul > li:nth-child(1) > a')
+                .should('exist')
+                .and('be.visible')
+                .click()
+                .then('select first product',()=>{
+                    cy.get('#center_column > ul > li:nth-child(1) > div > div.left-block > div > a.product_img_link > img')
+                        .first()
+                        .click({force:true})
+                })
+            
+            cy.get('#block_top_menu > ul > li:nth-child(1) > a')
+                .should('exist')
+                .and('be.visible')
+                .click()
+            
+            // importante find dentro de product container    
+            cy.get('.product-container')
+                .find(".product-image-container")
+                .eq(0)
+                .dblclick({force:true})
+
+                cy.get('#block_top_menu > ul > li:nth-child(1) > a')
+                .should('exist')
+                .and('be.visible')
+                .click()
+                .then('select first product',()=>{
+                    cy.get('#center_column > ul > li:nth-child(1) > div > div.left-block > div > a.product_img_link > img')
+                        .first()
+                        .click({force:true})
+                    cy.get("#product_condition .editable").then((e)=>{
+                        cy.log(e.text())
+                        if(e.text()==="New"){
+                            cy.log("El vestido es nuevo")
+                        }
+                    })
+                })
+
+                cy.get('#block_top_menu > ul > li:nth-child(1) > a')
+                .should('exist')
+                .and('be.visible')
+                .click()
+                .then('select first product',()=>{
+                    cy.get('#center_column > ul > li:nth-child(1) > div > div.left-block > div > a.product_img_link > img')
+                        .first()
+                        .click({force:true})
+                    cy.get("#our_price_display").then((e)=>{
+                        cy.log("El precio del vestido es: "+e.text()) 
+                    })
+                })
+            
+            
+            
+        })
 })
