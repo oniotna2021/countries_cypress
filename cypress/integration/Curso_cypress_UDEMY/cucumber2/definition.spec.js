7c707916c88f06212c08b3830e45e1826d18069f
oniotna2021 <tonyusb@gmail.com>
2022-04-17 22:19:21 -0500

    1704 2

---
+++ cypress/integration/Curso_cypress_UDEMY/cucumber2/definition.spec.js
@@ -0,0 +1,21 @@
+import { when,Given,And,Then } from "cypress/types/jquery"
+
+Given('open the browser',()=>{
+ 
+})
+
+when('input name',()=>{
+
+})
+
+when('input email',()=>{
+    
+})
+
+And('input address',()=>{
+    
+})
+
+Then('Title validation',()=>{
+    
+})
---
+++ cypress/integration/cucumber2.feature
@@ -0,0 +1,5 @@
+Feature: Cucumber dos demo
+
+    This is a demo, how to use cucumber
+
+        Scenario: Demo de cucumber 2
